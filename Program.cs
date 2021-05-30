a101d564b6e9a0ba35137924491694a276213404
Andres Sainz <andresmsainz@gmail.com>
2021-05-29 23:41:55 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericSortedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
