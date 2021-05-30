50cf236cd94e9d599aa13942a0a0f9f896edbf43
Andres Sainz <andresmsainz@gmail.com>
2021-05-29 23:51:03 -0400

    Add creating section

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
 
 namespace GenericSortedList
 {
@@ -6,7 +9,52 @@ namespace GenericSortedList
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Generic Sorted Lists!");
+
+            // Creating
+            Console.WriteLine("In Creating");
+
+            SortedList list = new SortedList(); // non generic sorted list
+            SortedList<int, string> phonebook = new SortedList<int, string>(); // generic sorted list
+
+            Console.WriteLine("------------");
+
+            // Adding
+            Console.WriteLine("In Adding");
+
+            Console.WriteLine("------------");
+
+            // Displaying
+            Console.WriteLine("In Displaying");
+
+            Console.WriteLine("------------");
+
+            // Index
+            Console.WriteLine("In Index");
+
+            Console.WriteLine("------------");
+
+            // TryGetValue
+            Console.WriteLine("In TryGetValue");
+
+            Console.WriteLine("------------");
+
+            // Contains
+            Console.WriteLine("In Contains");
+
+            Console.WriteLine("------------");
+
+            // Remove
+            Console.WriteLine("In Remove");
+
+            Console.WriteLine("------------");
+
+            // Remove All
+            Console.WriteLine("In Remove All");
+
+            Console.WriteLine("------------");
+
+
         }
     }
 }
