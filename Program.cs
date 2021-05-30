63079225eb69292debe1b1d6b76142fc06bdc462
Andres Sainz <andresmsainz@gmail.com>
2021-05-30 00:21:17 -0400

    Finishing Up

--- Program.cs
+++ Program.cs
@@ -43,25 +43,71 @@ namespace GenericSortedList
             // Index
             Console.WriteLine("In Index");
 
+            Console.WriteLine("Changing Jack's Number using Index");
+            phonebook["Jack"] = "(786)999-4387";
+
+            foreach (var item in phonebook)
+            {
+                Console.WriteLine("Current Item = " + item.Key + " , " + item.Value);
+            }
+
             Console.WriteLine("------------");
 
             // TryGetValue
             Console.WriteLine("In TryGetValue");
+
+            Console.WriteLine("Retrieving Willies Number");
+            phonebook.TryGetValue("Willie", out string result);
+            Console.WriteLine("Result = " + result);
+
+            Console.WriteLine("Retrieving Andres Number");
+            if (phonebook.TryGetValue("Andres", out string result2) == false) 
+            {
+                result2 = "Not Found";
+            }
+            Console.WriteLine("Result = " + result2);
 
             Console.WriteLine("------------");
 
             // Contains
             Console.WriteLine("In Contains");
 
+            Console.WriteLine("Looking for Jill");
+            Console.WriteLine("Does phone book contain the name Jill? = " + phonebook.ContainsKey("Jill")); 
+
+            Console.WriteLine("Looking for Pancho");
+            Console.WriteLine("Does phone book contain the name Pancho? = " + phonebook.ContainsKey("Pancho"));
+
+            Console.WriteLine("Looking for (212)444-4689");
+            Console.WriteLine("Does phone book contain the number (212)444-4689? = " + phonebook.ContainsValue("(212)444-4689"));
+
+            Console.WriteLine("Looking for (215)444-4689");
+            Console.WriteLine("Does phone book contain the number (215)444-4689? = " + phonebook.ContainsValue("(215)444-4689"));
+
             Console.WriteLine("------------");
 
             // Remove
             Console.WriteLine("In Remove");
 
+            Console.WriteLine("Removing Willie");
+            phonebook.Remove("Willie");
+
+            foreach (var item in phonebook)
+            {
+                Console.WriteLine("Current Item = " + item.Key + " , " + item.Value);
+            }
+
+
             Console.WriteLine("------------");
 
             // Remove All
             Console.WriteLine("In Remove All");
+            phonebook.Clear();
+
+            foreach (var item in phonebook)
+            {
+                Console.WriteLine("Current Item = " + item.Key + " , " + item.Value);
+            }
 
             Console.WriteLine("------------");
 
